00777aaa50119194f71eda3ad80ab175a2b65036
Denis Azhymov <azhimov93@gmail.com>
2017-01-13 10:40:28 +0200

    init

--- src/HomeWork/Task1.java
+++ src/HomeWork/Task1.java
@@ -1,6 +1,24 @@
 package HomeWork;
 
+import java.io.EOFException;
+
 public class Task1 {
+    public static void main(String[] args) throws Exception {
+
+        try {
+            System.out.println("Inside try block");
+            throw new EOFException("File is over");
+
+        } catch (EOFException ex) {
+            System.out.println(ex.getMessage());
+
+        } finally
+
+        {
+            System.out.println("Inside finally block");
+        }
+        System.out.println("After dangerous try-catch block");
+    }
 
 
 }
